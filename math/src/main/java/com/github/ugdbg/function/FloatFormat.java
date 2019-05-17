351324f8e17bc253bec4e2dc3cf2756cf726c509
hdg <hugues.dubourg@cspot.fr>
2019-05-17 13:45:45 +0200

    [ML] [MATH] [FLOAT] [FORMAT] A formatter that can format a float into a fixed length string. Useful to format matrices and vectors.

---
+++ math/src/main/java/com/github/ugdbg/function/FloatFormat.java
@@ -0,0 +1,70 @@
+package com.github.ugdbg.function;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * A formatter for Floats with a fixed length. 
+ * <br>
+ * Useful to format matrices or vector. 
+ * <br>
+ */
+public class FloatFormat {
+	private final int length;
+	private final String format;
+
+	/**
+	 * Default constructor : give me the number of digits and decimals.
+	 * <br>
+	 * Output length will always be digits + decimals +  
+	 * @param digits   the number of digits (before ".")
+	 * @param decimals the number of decimals (after ".")
+	 */
+	public FloatFormat(int digits, int decimals) {
+		this.format = "%1$+" + digits + "." + decimals + "f";
+		this.length = digits + decimals + 2;
+	}
+
+	/**
+	 * The output length of any float after it is formatted.
+	 * @return {@link #length}
+	 */
+	public int getLength() {
+		return this.length;
+	}
+
+	/**
+	 * Format a given float.
+	 * <ul>
+	 *     <li>The output length will ALWAYS be {@link #length}</li>
+	 *     <li>if the float cannot be formatted, a centered '!' is returned</li>
+	 *     <li>There is always a leading +/- sign</li>
+	 * </ul>
+	 * @param value the float value to format
+	 * @return the formatted value
+	 */
+	public String format(Object value) {
+		String out = String.format(this.format, value);
+		if (value != null && value.equals(Float.NEGATIVE_INFINITY)) {
+			out = "−∞";
+		}
+		
+		if (value != null && value.equals(Float.POSITIVE_INFINITY)) {
+			out = "+∞";
+		}
+		
+		out = StringUtils.center(out, this.length);
+		if (out.length() > this.length) {
+			out = StringUtils.center("!", this.length);
+		}
+		return out;
+	}
+
+	/**
+	 * Create a String of X spaces
+	 * @param amount the amout of spaces
+	 * @return a String of X spaces
+	 */
+	public String spaces(int amount) {
+		return new String(new char[Math.max(amount, 0)]).replace('\0', ' ');
+	}
+}
