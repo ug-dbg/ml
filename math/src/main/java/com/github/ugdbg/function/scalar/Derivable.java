29591156b8a98b575253a4d84622df7a7d4bd00c
hdg <hugues.dubourg@cspot.fr>
2019-05-17 14:30:12 +0200

    [ML] [MATH] [DERIVABLE] [VECTORIAL] a default method in the Derivable interface that can turn the current Derivable into a vectorial VDerivable equivalent.

--- math/src/main/java/com/github/ugdbg/function/scalar/Derivable.java
+++ math/src/main/java/com/github/ugdbg/function/scalar/Derivable.java
@@ -1,9 +1,33 @@
 package com.github.ugdbg.function.scalar;
 
+import com.github.ugdbg.function.vector.VDerivable;
+import com.github.ugdbg.function.vector.VFunction;
+
+/**
+ * An interface for ℝ functions that can be derived.
+ */
 public interface Derivable extends Function {
 	Function derive();
 	
 	default Derivable compose(Derivable with) {
 		return ComposedDerivable.of(this, with);
+	}
+	
+	/**
+	 * Get the vectorial function for this : f:x(x₁,x₂,x₃...xₘ) → y(y₁,y₂,y₃...yₙ) where yᵢ = this(xᵢ)   
+	 * @return a new vectorial function instance for this function 
+	 */
+	default VDerivable vectorial() {
+		return new VDerivable() {
+			@Override
+			public VFunction derive() {
+				return Derivable.this.derive().vectorial();
+			}
+
+			@Override
+			public float[] apply(float[] input) {
+				return Derivable.this.apply(input);
+			}
+		};
 	}
 }
